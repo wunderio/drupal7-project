4285c0c413a0545541ff05bc2b51aff6ebb13f29
Tormi Tabor <tormi.tabor@gmail.com>
2021-02-08 00:03:13 +0200

    Use www-data group in adminer's setup.

--- .lando/adminer.sh
+++ .lando/adminer.sh
@@ -8,5 +8,5 @@ cp -f /app/.lando/adminer.php /var/www/html/adminer.php
 cd /var/www/html
 find . -type f -name 'adminer-*' -print0 | xargs -0 -I{} cp -f {} adminer_library.php
 
-chown -R www-data:dialout /var/www/html
+chown -R www-data:www-data /var/www/html
 chmod -R +x /var/www/html
