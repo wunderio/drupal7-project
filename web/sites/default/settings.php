54d7a09d04229fc05b9769be5fa649c6ad5bc44c
Tormi Tabor <tormi.tabor@gmail.com>
2021-03-15 09:44:36 +0200

    Use unified settings.php.

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -5,21 +5,46 @@
  * General settings.
  */
 
-// The default list of directories that will be ignored by Drupal's file API.
+/**
+ * Database settings, overridden per environment.
+ */
+$databases = [];
+$databases['default']['default'] = [
+  'database' => $_ENV['DB_NAME_DRUPAL'],
+  'username' => $_ENV['DB_USER_DRUPAL'],
+  'password' => $_ENV['DB_PASS_DRUPAL'],
+  'host' => $_ENV['DB_HOST_DRUPAL'],
+  'port' => '3306',
+  'driver' => 'mysql',
+];
+
+/**
+ * Environment-specific settings.
+ */
+$env = $_ENV['ENVIRONMENT_NAME'];
+switch ($env) {
+  case 'production':
+    break;
+
+  case 'master':
+    break;
+
+  case 'local':
+    // Salt for one-time login links etc.
+    $drupal_hash_salt = md5(getenv('LANDO_HOST_IP'));
+    break;
+
+  default:
+    break;
+}
+
+/**
+ * The default list of directories that will be ignored by Drupal's file API.
+ */
 $conf['file_scan_ignore_directories'] = array(
   'node_modules',
   'bower_components',
 );
-
-// Lando configuration overrides.
-if (getenv('LANDO_INFO') && file_exists(__DIR__ . '/settings.lando.php')) {
-  include __DIR__ . '/settings.lando.php';
-}
-
-// Environment specific configuration, if available.
-if (file_exists(__DIR__ . '/settings.local.php')) {
-  include __DIR__ . '/settings.local.php';
-}
 
 /**
  * Silta cluster configuration overrides.
